06410e8261be59581933b3a41196a635496f9e76
siddharth_mishra <siddharth.mishra@globalpay.com>
2020-09-05 17:27:03 +0530

    Try and Error for SSL

--- install-site.sh
+++ install-site.sh
@@ -17,7 +17,7 @@ sudo apt update -y
 sudo apt install docker-ce -y
 
 #install docker compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
 
